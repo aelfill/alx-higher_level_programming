a8f3702e96d4973ad0c9aca28182b82a4c0cd7c7
aelfill <aelfil8@gmail.com>
2024-05-22 07:58:06 -0700

    README.md

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,15 +1,10 @@
--- creates the database hbtn_0d_usa and the table cities on your MySQL server.
-   -- cities description:
-      -- id INT unique - auto generated, cant be null, is a primary key
-      -- state_id INT - can't be null, must be a FOREIGN KEY that
-      --                references to id of the states table
-      -- name VARCHAR(256) - can't be null
-   -- If the table hbtn_0d_usa already exists, script should not fail
-   -- If the table cities already exists, script should not fail
+-- lists all the cities of California in the database hbtn_0d_usa
+   -- states table contains only one record where name = California
+   -- Results must be sorted in ascending order by cities.id
+   -- The database name will be passed as an argument of the mysql command
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-       state_id INT NOT NULL,
-       FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
-       name VARCHAR(256) NOT NULL);
+SELECT id, name FROM cities
+WHERE state_id = (
+      SELECT id FROM states
+      WHERE name = "California")
+ORDER BY id;
